249007fa5a291f28e2091c7e94910abc6ebc142a
elephantcastle <obymik89@gmail.com>
2021-01-30 23:10:45 +0100

    reintall prerender

--- src/main.js
+++ src/main.js
@@ -13,5 +13,6 @@ new Vue({
   store,
   vuetify,
   i18n,
-  render: h => h(App)
+  render: h => h(App),
+  mounted: () => document.dispatchEvent(new Event("x-app-rendered")),
 }).$mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -22,6 +22,20 @@ module.exports = {
       fallbackLocale: 'en',
       localeDir: 'locales',
       enableInSFC: true
+    },
+    prerenderSpa: {
+      registry: undefined,
+      renderRoutes: [
+        '/',
+        '/about',
+        '/policy',
+        '/intro',
+        '/results',
+        '/results/5febaa2cc9b7b116872e0cf2'
+      ],
+      useRenderEvent: true,
+      headless: true,
+      onlyProduction: true
     }
   }
 }
